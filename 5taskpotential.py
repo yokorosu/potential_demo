158de97ae44fb3858f87a6d439339cf207bddfe0
yokorosu <disorderss@mail.ru>
2024-03-29 17:02:54 +0300

    5 задание

---
+++ 5taskpotential.py
@@ -0,0 +1,28 @@
+import csv
+
+
+def generate_hash(s: str):
+    alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ '
+    d = {l: i for i, l in enumerate(alphabet, 1)}
+    p = 67
+    m = 1e9 + 9
+    hash_value = 0
+    p_pow = 1
+    for c in s:
+        hash_value = (hash_value + d[c] * p_pow) % m
+        p_pow = (p_pow * p) % m
+    return int(hash_value)
+
+
+student_with_hash = []
+with open('students.csv', encoding='utf-8') as file:
+    reader = list(csv.DictReader(file, delimiter=',', quotechar='"'))
+    for row in reader:
+        row['id'] = generate_hash(row['Name'])
+        student_with_hash.append(row)
+
+with open('students_with_hash.csv', 'w', newline='', encoding='utf-8') as file:
+    w = csv.DictWriter(file, fieldnames=['id', 'Name', 'titleProject_id', 'class', 'score', 'login', 'password'])
+    w.writeheader()
+    w.writerows(student_with_hash)
+
