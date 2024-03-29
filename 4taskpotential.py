6bb47737755bb8bc22228d22c210ef5a4527c97e
yokorosu <disorderss@mail.ru>
2024-03-29 17:02:47 +0300

    4 задание

---
+++ 4taskpotential.py
@@ -0,0 +1,28 @@
+import csv
+import string
+import random
+
+
+def create_initials(s: string):
+    names = s.split()
+    return f'{names[0]}_{names[1][0]}{names[2][0]}'
+
+
+def create_password():
+    characters = string.ascii_letters + string.digits
+    password = ''.join(random.choice(characters) for _ in range(8))
+    return password
+
+
+students_with_password = []
+with open('students.csv', encoding='utf-8') as file:
+    reader = list(csv.DictReader(file, delimiter=',', quotechar='"'))
+    for row in reader:
+        row['login'] = create_initials(row['Name'])
+        row['password'] = create_password()
+        students_with_password.append(row)
+
+with open('student_new.csv', 'w', newline='', encoding='utf-8') as file:
+    w = csv.DictWriter(file, fieldnames=['id', 'Name', 'titleProject_id', 'class', 'score', 'login', 'password'])
+    w.writeheader()
+    w.writerows(students_with_password)
